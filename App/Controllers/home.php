d9bb8dd14027009f1156858737077fdda37ddd24
Svilen <svilen.ivanov01@gmail.com>
2023-01-05 20:34:40 +0200

    Modified the App class. Added simple Home views

--- App/Controllers/home.php
+++ App/Controllers/home.php
@@ -3,8 +3,10 @@
 Class Home{
 
   function index(){
-
-    
+    echo "Home controller and index view";
+  }
+  function paper(){
+    echo "Home controller and paper view";
   }
 
 }
--- App/Core/app.php
+++ App/Core/app.php
@@ -1,31 +1,41 @@
 <?php
 
-Class App{
+Class App
+{
 
-  protected $controller = "home";
-  protected $method = "index";
-  protected $params = [];
-  public function __construct(){
-    $url = $this->splitURL();
-    $inputExists = "../app/controllers/". strtolower($url[0]) . ".php";
-    if (file_exists($inputExists)) {
-      $this->controller = (strtolower($url[0]));
-      unset($url[0]);
+	protected $controller = "home";
+	protected $method = "index";
+	Protected $params = [];
 
-    }
+	public function __construct()
+	{
+		$url = $this->splitURL();
 
-    require $inputExists;
-    $this->controller = new $this->controller;
+		if(file_exists("../App/Controllers/". strtolower($url[0]) . ".php"))
+		{
 
-    if(isset($url[1])){
-      if(method_exists($this->controller, $url[1])){
-        unset($url[1]);
-      }
-    }
-    show($url);
-  }
+			$this->controller = strtolower($url[0]);
+			unset($url[0]);
+		}
 
-  private function splitURL()
+		require "../App/Controllers/". $this->controller . ".php";
+		$this->controller = new $this->controller;
+
+		if(isset($url[1]))
+		{
+			if(method_exists($this->controller, $url[1]))
+			{
+				$this->method = $url[1];
+				unset($url[1]);
+			}
+		}
+
+		$this->params = array_values($url);
+		call_user_func_array([$this->controller,$this->method], $this->params);
+    //show($url);
+	}
+
+	private function splitURL()
 	{
 		$url = isset($_GET['url']) ? $_GET['url'] : "home";
 		return explode("/", filter_var(trim($url,"/"),FILTER_SANITIZE_URL));
